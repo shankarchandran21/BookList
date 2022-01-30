1b33ef85a7fe2300d851058eea974c13307a5711
Shankar Chandran <shankarchandran21@gmail.com>
2022-01-30 21:05:19 +0530

    Book-list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList(){
+  return(<section className='BookList'>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>);
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book =()=>{return(<article className='book'>
+  <Image/>
+  <Title/>
+  <Author/>
+</article>)};
+
+const Image=()=><img src='https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg'/>
+const Title=()=><h1>The Alchemist</h1>
+const Author=()=><h4>Paulo Coelho</h4>
+ReactDOM.render(<BookList/>,document.getElementById('root'))
