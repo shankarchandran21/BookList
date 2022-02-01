64b7055a844aac305f4a9d845ee523e01a26ccc4
Shankar Chandran <shankarchandran21@gmail.com>
2022-02-01 13:31:07 +0530

    Book-list-using-map-basic

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+const BookFullList=[
+  {
+Title:'The Alchemist',
+ Author:'Paulo Coelho',
+ Image:'https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg',
+},
+{
+ Title:'DO EPIC SHIT',
+  Author:'Ankur Warikoo',
+  Image:'https://images-eu.ssl-images-amazon.com/images/I/41+grDTP2FL._AC_UL604_SR604,400_.jpg',
+},
+]
 
 function BookList(){
   return(<section className='BookList'>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+    {BookFullList.map((book)=>{
+      const {Image,Title,Author}=book;
+      return<div>
+        <h3>{Title}</h3>
+        <h6>{Author}</h6>
+
+      </div>
+    })}
   </section>);
 };
 
-const Book =()=>{return(<article className='book'>
-  <Image/>
-  <Title/>
-  <Author/>
+const Book =(props)=>{
+  const{img,title,author,children}=props;
+  console.log(props)
+  return(<article className='book'>
+  <img src={img}/>
+  <h1>{title}</h1>
+  <h4>{author}</h4>
+  {children}
 </article>)};
 
-const Image=()=><img src='https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg'/>
-const Title=()=><h1>The Alchemist</h1>
-const Author=()=><h4>Paulo Coelho</h4>
 ReactDOM.render(<BookList/>,document.getElementById('root'))
